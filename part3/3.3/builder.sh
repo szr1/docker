017a91638209234d2f862dd442b0653c6291d480
Roland Szogi <roland.szogi@nixs.com>
2024-10-28 18:35:01 +0100

    added solutions to part 3

---
+++ part3/3.3/builder.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# check number of parameters
+if [ $# -lt 2 ]; then
+  echo 1>&2 "Not enough arguments."
+  echo 1>&2 "Usage: builder.sh <github-repo> <docker-hub-repo>"
+  exit 2
+fi
+
+# git clone https://github.com/$1.git
+github=$1
+
+# extract the last part as the sub-directory
+repo=$(basename "$github")
+
+# docker container tag as 2nd parameter
+tag=$2
+
+# clean old directory
+if [ -d "$repo" ]; then
+   echo "found directory $repo. Deleting ..."
+   rm -Rf $repo;
+fi
+
+# fetch repo
+echo "cloning repo $1"
+git clone https://github.com/$1.git
+
+# build docker
+echo "building docker image ..."
+docker build -t $tag $repo
+
+# publish on docker hub
+echo "pushing image ..."
+docker push $tag
+
+exit 0
